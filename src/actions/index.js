0d08f43ca536ed1c5d9241279e624a13e307b68e
aliaz9 <alina.alvarez9@gmail.com>
2023-02-14 12:01:35 -0300

    Arreglo de la action para que funcione JSON

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,37 +4,14 @@ export const GET_PRODUCTS = "GET_PRODUCTS";
 
 export default function getProducts() {
 
-    // ELIMINO ESTE OBJETO CUANDO TENGA LA RUTA (Alina)
-    let ejemplo = [{
-        "id": 1,
-        "title": "Panel Solar Fotovoltaico Policristalino Jinko Solar Eagle 72 celdas 335Wp",
-        "price": "$2000",
-        "score": "https://static.vecteezy.com/system/resources/previews/004/208/177/non_2x/stars-rating-review-set-of-black-stars-free-vector.jpg",
-        "image": "https://pansogal.com/wp-content/uploads/2019/07/funciona-panel-solar-con-nubes-1000x900.jpg"
-    },
-    {
-        "id": 2,
-        "title": "Panel Dos Fotovoltaico Policristalino Jinko Solar Eagle 72 celdas 335Wp",
-        "price": "$2000",
-        "score": "https://static.vecteezy.com/system/resources/previews/004/208/177/non_2x/stars-rating-review-set-of-black-stars-free-vector.jpg",
-        "image": "https://pansogal.com/wp-content/uploads/2019/07/funciona-panel-solar-con-nubes-1000x900.jpg"
-    },
-    {
-        "id": 3,
-        "title": "Panel Tres Fotovoltaico Policristalino Jinko Solar Eagle 72 celdas 335Wp",
-        "price": "$2000",
-        "score": "https://static.vecteezy.com/system/resources/previews/004/208/177/non_2x/stars-rating-review-set-of-black-stars-free-vector.jpg",
-        "image": "https://pansogal.com/wp-content/uploads/2019/07/funciona-panel-solar-con-nubes-1000x900.jpg"
-    }]
-    
-
     return async function (dispatch){
 
-    //const json = await axios.get("");
+    const json = await axios.get("https://run.mocky.io/v3/dd8097a8-8336-4f15-9237-64f72472faa1");
+    //console.log(json.data)
     
     return dispatch({
     type: "GET_PRODUCTS",
-    payload: ejemplo,
+    payload: json.data,
     })
 }
 };
