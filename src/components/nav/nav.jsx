31632d13355c91f730a2c1d5e9cd68fd359eddb3
tomicicolini <110571944+tomicicolini@users.noreply.github.com>
2023-02-15 16:46:39 -0300

    creacion nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import './nav.css';
 
 
-export default function Nav() {
+export default function Nav () {
     return (
-        <div>Primer Componente Nav!</div>
+        <div className= "nav">
+            <div className="nav1">
+            <Link to="/home">
+                <button>Inicio</button>
+            </Link>
+            <Link to="/about">
+                <button>Acerca de</button>
+            </Link>
+            <Link to="/products">
+                <button>Productos</button>
+            </Link>
+            </div>
+            <div className="nav2">
+            <Link to="/log In">
+                <button>Iniciar sesión</button>
+            </Link>
+            <Link to="/sign in">
+                <button>Registrarse</button>
+            </Link>
+            </div>
+            <div className="nav3">
+            <Link to="/cart">
+                <img src="https://tse2.mm.bing.net/th?id=OIP.ZTkFCCP4CmG0YtHR9umHdAHaHa&pid=Api&P=0" width={30} height={30} />
+            </Link>
+            </div>
+
+        </div>
     )
 }
+
