e8339aaf3736e19e4fdbe704a0ffc918645974e6
tomicicolini <110571944+tomicicolini@users.noreply.github.com>
2023-02-16 09:42:59 -0300

    creacion nav

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Cards from './components/cards/cards';
 import ProductPage from './components/product-page/product-page';
+import Nav from './components/nav/nav';
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/" component={Nav}></Route>
         <Route exact path='/' component={Cards}></Route>
         <Route exact path='/product' component={ProductPage}></Route>
       </Switch>
