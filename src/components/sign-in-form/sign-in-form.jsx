b765cc265f08233cf09157aed7da74eff5d00709
aliaz9 <alina.alvarez9@gmail.com>
2023-02-19 09:40:36 -0300

    Implementación de Formik

--- src/components/sign-in-form/sign-in-form.jsx
+++ src/components/sign-in-form/sign-in-form.jsx
@@ -1,61 +1,75 @@
 import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useDispatch } from "react-redux";
 import { addUser } from "../../actions";
-import '../sign-in-form/sign-in-form.css'
-import { Form } from "react-bootstrap";
-
-
-const user = {
-    name: "Alina",
-    email: "alina@gmail.com",
-    password: "Cafe123",
-    google: false
-} 
-
-export default function SignIn () {
+import { Button } from "react-bootstrap";
+import '../sign-in-form/sign-in-form.css';
 
 
 
-    let [input, setInput] = useState({
-        name: "",
-        email: "",
-        password: ""
-    })
-    let dispatch = useDispatch();
+export default function SignIn() {
 
-    function handleChange(e) {
-    e.preventDefault();
-    setInput({
-        ...input,
-        [e.target.name]: e.target.value,
-    })
-    console.log(input)
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log("entra al submit", input)
-        dispatch(addUser(input))
-      }
+let dispatch = useDispatch();
+let [ success, setSuccess ] = useState(false);
 
     return (
-          <form>
-            <h1> Sign In:</h1>
-            <div>
-            <label>Name:</label>
-            <input placeholder="Write your name" name="name" type="text" onChange={(e) => handleChange(e)}></input>
-            </div>
+        <div className="box">
+            <h1>Registrate!</h1>
+            <Formik
+                initialValues={{ name: '', email: '', password: '' }}
 
-            <div>
-            <label>Email:</label>
-            <input placeholder="Write your email" name="email" type="text" onChange={(e) => handleChange(e)}></input>
-            </div>
+                validate={values => {
+                    const errors = {};
 
-            <div>
-            <label>Password:</label>
-            <input placeholder="Write your password" name="password" type="password" onChange={(e) => handleChange(e)}></input>
-            </div>
-            <button onClick={(e) => handleSubmit(e)}>SIGN UP</button>
-            </form>
+                    if (!values.email) {
+                        errors.email = 'Required';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = 'Invalid email address';
+                    }
+                    
+                    return errors;
+                }}
+
+            onSubmit = {(values, {resetForm}) => {
+                resetForm();
+                dispatch(addUser(values));
+                setSuccess(true);
+                setTimeout(() => setSuccess(false), 5000)
+            }}
+
+            >
+                {( {values, touched , handleSubmit, handleChange, handleBlur} ) => (
+                    <Form onSubmit={handleSubmit}>
+                        <div class="mb-3">
+                        <label class="form-label">Nombre:</label>
+                        <Field type="text" name="name" class="form-control"/>
+                        { touched.name && <ErrorMessage name="name" component="div" /> }
+                        </div>
+
+                        <div class="mb-3">
+                        <label class="form-label">Email:</label>
+                        <Field type="email" name="email" class="form-control"/>
+                        { touched.email && <ErrorMessage name="email" component="div" /> }
+                        </div>
+
+                        <div class="mb-3">
+                        <label class="form-label">Contraseña:</label>
+                        <Field type="password" name="password" class="form-control"/>
+                        { touched.password && <ErrorMessage name="password" component="div" /> }
+                        </div>
+
+                        <button type="submit" class="btn btn-primary mb-3">
+                            Regitrarse
+                        </button>
+
+                        { success && <p> Su Usuario ha sido registrado con éxito. </p> }
+
+                    </Form>
+                )}
+            </Formik>
+        </div>
     )
-}
+};
+
