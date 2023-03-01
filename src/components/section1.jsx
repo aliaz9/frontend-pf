038e49d43a5f6b0be6d288f58e2843c40730549f
zoma <zotelolm@gmail.com>
2023-02-28 19:18:37 -0600

    maybe do this

--- src/components/section1.jsx
+++ src/components/section1.jsx
@@ -1,11 +1,10 @@
 import Style from '../styles/section1.module.css'
 import { Link } from 'react-router-dom'
 
-
-export default function Section1 () {
+export default function Section1() {
   return (
     <div className={Style.container}>
-        <h1>hols</h1>
+      <h1>hols</h1>
     </div>
-
-  )}
+  )
+}
