397d6a8bf21ea316b68c8028c02589bc9d1ee0c9
Nir Raschi <99211017+nirraschi@users.noreply.github.com>
2023-03-06 12:15:41 -0300

    Cambios sin nav ni scrollAnimation

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer.jsx'
 import Nav from '../components/Nav.jsx'
+import Style from '../styles/Layout.module.css'
 
 export default function Layout () {
   return (
-    <div>
+    <div className={Style.layoutBody}>
       <Nav/>
       <Outlet/>
       <Footer/>
--- src/components/section1.jsx
+++ src/components/section1.jsx
@@ -5,7 +5,20 @@ import { Link } from 'react-router-dom'
 export default function Section1 () {
   return (
     <div className={Style.container}>
-        <h1>hols</h1>
+      <div className={Style.izquierda}>
+         <h1>¿Qué ventajas tiene la Energía Solar?</h1>
+         <h5>Los paneles solares son una buena alternativa si quieres consumir energía propia, limpia y ahorrar dinero a largo plazo:</h5>
+        <ul>
+          <li><i class="fa-solid fa-leaf"></i>Cero emisiones contaminantes</li>
+          <li><i class="fa-solid fa-leaf"></i>Ahorro energético</li>
+          <li><i class="fa-solid fa-leaf"></i>No genera ruido</li>
+        </ul>
+
+      </div>
+
+      <div className={Style.derecha}>
+         <img className={Style.image} src="https://res.cloudinary.com/daieynebb/image/upload/v1678044606/Fondos/section1_zbmi5i.png" alt="" />
+      </div>
     </div>
 
   )}
