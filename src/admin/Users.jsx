847e602d76d202c22a1e5d2c82afa3442ea937c9
aliaz9 <alina.alvarez9@gmail.com>
2023-03-07 13:33:43 -0300

    Usuarios Habilitados

--- src/admin/Users.jsx
+++ src/admin/Users.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import styles from '../styles/Users.module.css'
-import { deleteUser, users } from '../redux/slices/thunksAdmin'
+import { deleteUser, habilitarUser, users } from '../redux/slices/thunksAdmin'
 
 export default function Users() {
   const allUsers = useSelector((state) => state.admin.allUsers)
   const dispatch = useDispatch()
+
 
   useEffect(() => {
     dispatch(users())
@@ -15,37 +16,60 @@ export default function Users() {
     dispatch(deleteUser(id))
   }
 
+  function handleHabilitar(id) {
+    dispatch(habilitarUser(id))
+  }
+
   return (
     <div>
       <h1 className={styles.title}>Usuarios</h1>
 
       <div className={`${styles.container} container`}>
         <div className={`${styles.row} row`}>
-          <div className="col-3">Nombre</div>
-          <div className="col-3">Email</div>
+          <div className="col-2">Nombre</div>
+          <div className="col-2">Email</div>
           <div className="col-2">Ordenes</div>
-          <div className="col-2">Eliminar</div>
+          <div className="col-2">Estado</div>
+          <div className="col-2">Acciones</div>
 
           {allUsers &&
             allUsers.map((u, indice) => {
               return (
                 <div key={indice} className={`${styles.row} row`}>
-                  <div className="col-3">{u.name}</div>
-                  <div className="col-3">{u.email}</div>
+                  <div className="col-2">{u.name}</div>
+                  <div className="col-2">{u.email}</div>
                   <div className="col-2">
                     <button className={`${styles.blue} btn btn-primary`}>
                       {' '}
                       Ver Ordenes{' '}
                     </button>
                   </div>
+
                   <div className="col-2">
-                    <button
-                      className={`${styles.red} btn btn-danger`}
-                      onClick={() => handleDelete(u.uid)}
-                    >
-                      {' '}
-                      Borrar{' '}
-                    </button>
+                    {  u.disabled ? <p style={{ color: '#00ff00' }}>Activo</p> : <p style={{ color: '#ff0000' }}>Inactivo</p> }
+                  </div>
+
+                  <div className="col-2">
+                    {
+                      u.disabled ?
+
+                        <button
+                          className={`${styles.red} btn btn-danger`}
+                          onClick={() => handleDelete(u.uid)}
+                        >
+                          {' '}
+                          Deshabilitar{' '}
+                        </button>
+                        :
+                        <button
+                          className={`${styles.red} btn btn-secondary`}
+                          onClick={() => handleHabilitar(u.uid)}
+                        >
+                          {' '}
+                          Habilitar{' '}
+                        </button>
+                    }
+
                   </div>
                 </div>
               )
--- src/redux/slices/thunksAdmin.js
+++ src/redux/slices/thunksAdmin.js
@@ -6,7 +6,8 @@ import {
   getUsers,
   getOrders,
   eliminateUser,
-  setOrderDetail
+  setOrderDetail,
+  habilitarUsuario
 } from './adminSlice.js'
 
 const config = {
@@ -19,7 +20,10 @@ const config = {
 export const users = () => {
   return async (dispatch) => {
     try {
-      const { data } = await clientAxios('/admin/users', config)
+      //const { data } = await clientAxios('/admin/users', config)
+      const { data } = await axios.get("https://run.mocky.io/v3/6d49b3ec-6ba1-4daf-98d8-92178fd8ac32")
+      
+
       dispatch(getUsers(data))
     } catch (error) {
       dispatch(setMessage({ error: error.response.data }))
@@ -46,6 +50,18 @@ export const deleteUser = (id) => {
     try {
       await clientAxios.delete(`/admin/delete-user/${id}`, config)
       dispatch(eliminateUser(id))
+    } catch (error) {
+      console.log(error)
+      dispatch(setMessage({ error: error.message }))
+    }
+  }
+}
+
+export const habilitarUser = (id) => {
+  return async (dispatch) => {
+    try {
+      // await clientAxios.delete(`//${id}`, config)
+      dispatch(habilitarUsuario(id))
     } catch (error) {
       console.log(error)
       dispatch(setMessage({ error: error.message }))
